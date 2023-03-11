227764bd9d33b4e7ab797a1fe163fe1f89a47c02
Lewynation <87381730+Lewynation@users.noreply.github.com>
2023-03-11 16:45:02 +0300

    Update vite.config.ts

--- website-dashboard/vite.config.ts
+++ website-dashboard/vite.config.ts
@@ -5,5 +5,5 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-//   base: "/typesense-admin-dashboard/",
+  base: "/typesense-admin-dashboard/",
 });
