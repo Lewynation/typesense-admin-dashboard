cb847ee14dc0c618292fdabea962bae0f72fc2f1
Lewynation <lewisotieno29@gmail.com>
2023-03-14 23:20:46 +0300

    containerization

--- website-dashboard/vite.config.ts
+++ website-dashboard/vite.config.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), svgr()],
-  base: "/typesense-admin-dashboard/",
-});
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd(), "") };
+  return defineConfig({
+    plugins: [react(), svgr()],
+    base: process.env.BASE_URL,
+  });
+};
