35d28e5586873db9bf1015635c4ca51cf2634f5d
Lewynation <lewisotieno29@gmail.com>
2023-04-22 20:07:57 +0300

    tailwind config

--- apps/website_dashboard/next.config.js
+++ apps/website_dashboard/next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  transpilePackages: ["ui"],
   experimental: {
     appDir: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ packages/ui/lib/Button.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export const Button = () => {
+  return (
+    <button className="bg-blue-500 font-bold p-3 rounded-lg">Click me</button>
+  );
+};
